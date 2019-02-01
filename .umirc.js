4cd3e7318091e432c837e545b797d0323ff4a118
NgeKaworu <NgeKaworu@163.com>
2019-02-01 20:48:34 +0800

    基础配置信息

--- .umirc.js
+++ .umirc.js
@@ -1,3 +1,24 @@
 export default {
-  plugins: ['umi-plugin-dva'],
+  plugins: [
+    [
+      'umi-plugin-react',
+      {
+        dva: true,
+        antd: true,
+        dynamicImport: {
+          webpackChunkName: true,
+        },
+      }
+    ],
+  ],
+  proxy: {
+    '/api': {
+      'target': "http://localhost:5000",
+      "changeOrigin": true,
+      "pathRewrite": { "^/api": "" }
+    }
+  },
+  base: '/blog/',
+  publicPath: '/blog/',
+  history: 'hash',
 }
